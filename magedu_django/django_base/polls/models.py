96105be4582b8f6e755c08c5a102668e0edeb660
Szzhe <szz1298@126.com>
2017-12-17 22:25:53 +0800

    django

---
+++ magedu_django/django_base/polls/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateField('date published')
+
+    class Meta:
+        db_table = "question"
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'choice'
